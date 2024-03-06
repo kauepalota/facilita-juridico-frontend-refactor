4b57559d0f0df72e8cf121b20c198d0b014aab3e
kauepalota <kurpfy@gmail.com>
2024-03-05 22:18:13 -0300

    lint: add prettier to order tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,22 @@ import { SearchIcon, Plus, Waypoints } from 'lucide-react'
 
 export default function Home() {
   return (
-    <div className="p-6 max-w-4xl space-y-4 mx-auto">
+    <div className="mx-auto max-w-4xl space-y-4 p-6">
       <h1 className="font-semibold">Clientes</h1>
 
       <div className="flex items-center justify-between">
         <form className="flex items-center gap-2">
           <input
-            className="h-9 w-full border border-stone-200 bg-transparent rounded px-3 py-1 text-sm shadow-sm outline-none"
+            className="h-9 w-full rounded border border-stone-200 bg-transparent px-3 py-1 text-sm shadow-sm outline-none"
             placeholder="ID do cliente"
           />
           <input
-            className="h-9 w-full border border-stone-200 bg-transparent rounded px-3 py-1 text-sm shadow-sm outline-none"
+            className="h-9 w-full rounded border border-stone-200 bg-transparent px-3 py-1 text-sm shadow-sm outline-none"
             placeholder="Nome do cliente"
           />
           <button
-            className="flex gap-2 text-stone-950 shadow-sm h-9 px-2 py-2 items-center whitespace-nowrap 
-          rounded-md text-sm font-medium bg-stone-100 hover:bg-stone-50"
+            className="flex h-9 items-center gap-2 whitespace-nowrap rounded-md bg-stone-100 px-2 py-2 
+          text-sm font-medium text-stone-950 shadow-sm hover:bg-stone-50"
           >
             <SearchIcon size={16} />
             <span className="max-md:hidden">Filtrar resultados</span>
@@ -28,15 +28,15 @@ export default function Home() {
 
         <div className="ml-2 flex items-center gap-2">
           <button
-            className="flex gap-2 text-white bg-stone-950 shadow h-9 px-2 py-2 items-center whitespace-nowrap 
-        rounded-md text-sm font-medium hover:bg-stone-900"
+            className="flex h-9 items-center gap-2 whitespace-nowrap rounded-md bg-stone-950 px-2 py-2 text-sm 
+        font-medium text-white shadow hover:bg-stone-900"
           >
             <Plus size={16} />
             <span className="max-md:hidden">Novo cliente</span>
           </button>
           <button
-            className="flex gap-2 text-white bg-stone-950 shadow h-9 px-2 py-2 items-center whitespace-nowrap 
-        rounded-md text-sm font-medium hover:bg-stone-900"
+            className="flex h-9 items-center gap-2 whitespace-nowrap rounded-md bg-stone-950 px-2 py-2 text-sm 
+        font-medium text-white shadow hover:bg-stone-900"
           >
             <Waypoints size={16} />
             <span className="max-md:hidden">Calcular rota</span>
@@ -44,7 +44,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="border rounded-lg">
+      <div className="rounded-lg border">
         <ClientTable />
       </div>
     </div>
