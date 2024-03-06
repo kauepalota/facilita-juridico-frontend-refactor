966e3abebeee7af8be7e2ec404a31e883db4b4a8
kauepalota <kurpfy@gmail.com>
2024-03-05 21:28:13 -0300

    feat: initial commit

---
+++ src/app/components/ClientTable/index.tsx
@@ -0,0 +1,30 @@
+export default function ClientTable() {
+  return (
+    <table className="w-full overflow-auto text-sm">
+      <thead>
+        <tr className="border-b">
+          <th className="h-10 px-2 text-left align-middle font-medium">ID</th>
+          <th className="h-10 px-2 text-left align-middle font-medium">
+            Cliente
+          </th>
+          <th className="h-10 px-2 text-left align-middle font-medium">
+            Localização
+          </th>
+        </tr>
+      </thead>
+
+      <tbody className="[&_tr:last-child]:border-0">
+        <tr className="border-b">
+          <td className="p-2 align-middle">#1</td>
+          <td className="p-2 align-middle">João</td>
+          <td className="p-2 align-middle">Rio de Janeiro</td>
+        </tr>
+        <tr className="border-b">
+          <td className="p-2 align-middle">#2</td>
+          <td className="p-2 align-middle">Maria</td>
+          <td className="p-2 align-middle">São Paulo</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
